f138eaf25a7d647996ca396ffad61481d000265a
Ed <eddie0lin@hotmail.com>
2020-11-30 22:22:55 +0800

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by User on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
